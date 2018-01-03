b6c2e7d9b02d05b34b10460ac12e9b7466e5a80e
Vinay <vinay@gmail.com>
2018-01-03 05:29:24 -0500

    java file modified

--- First.java
+++ First.java
@@ -3,7 +3,7 @@ public class First{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 6; i++) {
-			System.out.println("Welcome to MDM");
+			System.out.println("Welcome to MDM World!");
 		}
 	}
 
